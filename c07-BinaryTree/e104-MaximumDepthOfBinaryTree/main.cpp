ef90917ed15e4c50f6b7c4738eb262cb25411f0d
xcsinhome <evtjk@hotmail.com>
2025-02-06 23:25:38 +0800

    整理 整理二叉树相关

---
+++ c07-BinaryTree/e104-MaximumDepthOfBinaryTree/main.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+// #include <iostream>
+#include <queue>
+#include "../../leetcodeStruct/treenode.h"
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        
+    }
+};
+
+int main() {
+    return 0;
+}
---
+++ c07-BinaryTree/e144-BinaryTree-PreorderTraversal/preorder_tree.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include "../leetcodeStruct/treenode.h"
+
+class Solution_recur {
+int recur(TreeNode* node,std::vector<int>& vec){
+    if(node){
+        vec.push_back( node->val);
+        recur(node->left,vec);
+        recur(node->right,vec);
+    }
+    return 0;
+}
+public:
+    std::vector<int> preorderTraversal(TreeNode* root) {
+        std::vector<int> vec;
+        std::stack<int> sta;
+        return vec;
+    }
+};
+
+class Solution {
+int recur(TreeNode* node,std::vector<int>& vec){
+    if(node){
+        vec.push_back( node->val);
+        recur(node->left,vec);
+        recur(node->right,vec);
+    }
+
+    
+    return 0;
+}
+
+
+public:
+    std::vector<int> preorderTraversal(TreeNode* root) {
+        std::vector<int> vec;
+        std::stack<int> sta;
+        return vec;
+    }
+};
+
+int main(){
+    TreeNode node;
+    node.val = 1;
+    std::cout<<"sasda\n";
+    return 0;
+}
