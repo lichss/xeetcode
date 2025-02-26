7c64d42ef5d708965b14d8de41313313225e1a77
xcs <evtjk@hotmail.com>
2025-02-26 15:42:17 +0800

    子集弍 是个人物 以后要复习

--- c08-Backtracking/e078-Subsets/main.cpp
+++ c08-Backtracking/e078-Subsets/main.cpp
@@ -5,7 +5,42 @@
 #include <algorithm>
 // #include "../leetcodeStruct/treenode"
 
+using std::vector;
 
+class Solution {
+
+private:
+    std::vector<int> path;
+    std::vector<vector<int>> combs = {};
+    std::vector<int> nums;
+
+    void backtrack(int startIndex){
+
+        // if(startIndex == nums.size()){
+        //     combs.push_back(path);
+        //     return;
+        // } 
+
+        int newNum;
+        for(int i=startIndex;i<nums.size();i++){
+            newNum = nums[i];
+            path.push_back(newNum);
+            backtrack(i+1);
+            combs.push_back(path);
+            path.pop_back();
+
+        }
+
+        return;
+    }
+
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        this->nums = nums;
+        backtrack(0);
+        return combs;
+    }
+};
 
 int main() {
 
